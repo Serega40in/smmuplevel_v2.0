5afcb3256a2bf029d5d4e213c7c7e4b1b0cc6917
serega40in <serega40-in@yandex.ru>
2018-09-12 22:32:55 +0300

    Merge popup and some mobile changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # smmuplevel
 
 Project for instagram advetising studio.
+
+https://serega40in.github.io/smmuplevel_v2/
--- js/index40in.js
+++ js/index40in.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
+    PopUpHideTry();
+    PopUpHideYes();
+    PopUpHideNo();
     PopUpHide1();
     PopUpHide2();
     PopUpHide3();
     PopUpHide4();
     PopUpHide5();
 });
+function PopUpShowTry(){  
+    $("#popup_try").show();    
+}
+function PopUpHideTry(){
+    $("#popup_try").hide();
+}
+function PopUpShowYes(){  
+    $("#popup_yes").show();    
+}
+function PopUpHideYes(){
+    $("#popup_yes").hide();
+}
+function PopUpShowNo(){  
+    $("#popup_no").show();    
+}
+function PopUpHideNo(){
+    $("#popup_no").hide();
+}
 function PopUpShow1(){	
     $("#tariff_popup1").show();
 }
