cee29db7c64a43f8dcb79b929238d07a02e1a68b
serega40in <serega40-in@yandex.ru>
2018-09-21 01:42:33 +0300

    I think near the end. Now need Testing and optimization.

--- js/index40in.js
+++ js/index40in.js
@@ -9,49 +9,54 @@ $(document).ready(function(){
     PopUpHide5();
 });
 function PopUpShowTry(){  
-    $("#popup_try").show();    
+    $("#popup_try").css("display", "flex");
 }
 function PopUpHideTry(){
     $("#popup_try").hide();
 }
 function PopUpShowYes(){  
-    $("#popup_yes").show();    
+    $("#popup_yes").css("display", "flex");    
 }
 function PopUpHideYes(){
     $("#popup_yes").hide();
 }
 function PopUpShowNo(){  
-    $("#popup_no").show();    
+    $("#popup_no").css("display", "flex");    
 }
 function PopUpHideNo(){
     $("#popup_no").hide();
 }
 function PopUpShow1(){	
-    $("#tariff_popup1").show();
+    $("#tariff_popup1").css("display", "flex");
+    document.location.href = "#tar";
 }
 function PopUpHide1(){
     $("#tariff_popup1").hide();
 }
 function PopUpShow2(){
-    $("#tariff_popup2").show();
+    $("#tariff_popup2").css("display", "flex");
+    document.location.href = "#tar";
 }
 function PopUpHide2(){
     $("#tariff_popup2").hide();
 }
 function PopUpShow3(){
-    $("#tariff_popup3").show();
+    $("#tariff_popup3").css("display", "flex");
+    document.location.href = "#tar";
 }
 function PopUpHide3(){
     $("#tariff_popup3").hide();
 }
 function PopUpShow4(){
-    $("#tariff_popup4").show();
+    $("#tariff_popup4").css("display", "flex");
+    document.location.href = "#tar";
 }
 function PopUpHide4(){
     $("#tariff_popup4").hide();
 }
 function PopUpShow5(){	
-    $("#tariff_popup5").show();    
+    $("#tariff_popup5").css("display", "flex");    
+    document.location.href = "#tar";
 }
 function PopUpHide5(){
     $("#tariff_popup5").hide();
